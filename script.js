5027528ebf93461f666a303f1f5b64b8adee143f
srisaranmurugan <srisaranmurugan@admindroid.dev>
2026-03-28 14:15:33 +0530

    Revert "-> Issue fix: Variables were defined after the functions which uses it, so error occured."
    This reverts commit 2e33dfdf0c6ddd62a6f59f6edbe1e84f7584e38f.

--- script.js
+++ script.js
@@ -1,43 +1,4 @@
 const playPauseBtn = document.querySelector(".playPause");
-const prevBtn = document.querySelector(".prevBtn");
-const nextBtn = document.querySelector(".nextBtn");
-const title = document.querySelector(".song-info h2");
-const artist = document.querySelector(".artist");
-const cover = document.querySelector(".image-box img");
-const audio = document.getElementById("audio");
-const songEndTime = document.querySelector(".endTime");
-let currentSong = 0;
-
-const songs = [
-  {
-    title: "Sachein theme music",
-    artist: "Artist One",
-    cover: "image/Km.png",
-    audio: "audio/Km.mp3",
-    endTime: "0:51"
-  },
-  {
-    title: "Thug life",
-    artist: "Artist Two",
-    cover: "image/thuglife.png",
-    audio: "audio/Letsplay.mp3",
-    endTime: "2:44"
-  },
-  {
-    title: "Song three",
-    artist: "Artist Three",
-    cover: "image/folk.png",
-    audio: "audio/tm.mp3",
-    endTime: "1:50"
-  },
-  {
-    title: "Song four",
-    artist: "Artist Four",
-    cover: "image/musicpic.png",
-    audio: "audio/njm.mp3",
-    endTime: "5:15"
-  }
-];
 
 playPauseBtn.addEventListener("click", () => {
     if (audio.paused) {
@@ -48,11 +9,46 @@ playPauseBtn.addEventListener("click", () => {
     playPauseBtn.textContent = "▶";
   }
 });
+const songs = [
+  {
+    title: "Song One",
+    artist: "Artist One",
+    cover: "Km.png",
+    audio: "Km.mp3"
+  },
+  {
+    title: "Song Two",
+    artist: "Artist Two",
+    cover: "thuglife.png",
+    audio: "Letsplay.mp3"
+  },
+    {
+    title: "Song three",
+    artist: "Artist Three",
+    cover: "folk.png",
+    audio: "tm.mp3"
+  }
+];
 
+const title = document.querySelector(".song-info h2");
+const artist = document.querySelector(".artist");
+const cover = document.querySelector(".image-box img");
+const audio = document.getElementById("audio");
+
+let currentSong = 0;
+
+function loadSong(index) {
+  const song = songs[index];
+
+  title.textContent = song.title;    
+  artist.textContent = song.artist;   
+  cover.src = song.cover;             
+  audio.src = song.audio;             
+}
 prevBtn.addEventListener("click", () => {
-  
+  currentSong--;
+
   if (currentSong < 0) {
-    currentSong--;
     currentSong = songs.length - 1;
   }
 
@@ -60,16 +56,4 @@ prevBtn.addEventListener("click", () => {
   audio.play();
   playPauseBtn.textContent = "⏸";
 });
-
-function loadSong(index) {
-  const song = songs[index];
-  title.textContent = song.title;    
-  artist.textContent = song.artist; 
-  songEndTime.textContent = song.endTime;  
-  cover.src = song.cover;             
-  audio.src = song.audio; 
-  audio.play();            
-}
-
-loadSong(0);
 
